7682a20afb7f52ad1d6a15d3a35ecbac659adff3
dave-evans-dmv <dave.evans@dmv.virginia.gov>
2025-09-25 10:37:27 -0400

    Added Likes functionality and started development on comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,125 @@
 using System.Diagnostics;
-using DMV_Connect.Models;
+using DMV_Connect.ViewModels.Home;
+using DMVConnect.Data;
+using DMVConnect.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DMV_Connect.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _context.Posts
+                .Include(n => n.User)
+                .Include(l => l.Likes)
+                .Include(c => c.Comments)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+
+            return View(allPosts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(PostVM post)
+        {
+            // Get Logged in user
+            int loggedInUser = 2;
+
+            //Create new post
+            var newPost = new Post
+            {
+                Content = post.Content,
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                ImageUrl = "",
+                NrOfReports = 0,
+                UserId = loggedInUser
+            };
+
+            // Check and save image
+            if (post.Image != null && post.Image.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                if (post.Image.ContentType.Contains("image"))
+                {
+                    string rootFolderPathImages = Path.Combine(rootFolderPath, "images/uploaded");
+                    Directory.CreateDirectory(rootFolderPathImages);
+
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(post.Image.FileName);
+                    string filePath = Path.Combine(rootFolderPathImages, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        await post.Image.CopyToAsync(stream);
+
+                    // Set the URL to the newPost object
+                    newPost.ImageUrl = "/images/uploaded/" + fileName;
+                }
+            }
+
+            await _context.Posts.AddAsync(newPost);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TogglePostLike(PostLikeVM postLikeVM)
+        {
+            int loggedInUserId = 2;
+
+            // Check if user has already liked the post
+            var like = await _context.Likes
+                .Where(l => l.PostId == postLikeVM.PostId && l.UserId == loggedInUserId)
+                .FirstOrDefaultAsync();
+
+            if (like != null)
+            {
+                _context.Likes.Remove(like);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newLike = new Like()
+                {
+                    PostId = postLikeVM.PostId,
+                    UserId = loggedInUserId
+                };
+                await _context.Likes.AddAsync(newLike);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPostComment(PostCommentVM postCommentVM)
+        {
+            int loggedInUserId = 2;
+
+            var newComment = new Comment()
+            {
+                PostId = postCommentVM.PostId,
+                UserId = loggedInUserId,
+                Content = postCommentVM.Content,
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now
+            };
+
+            await _context.Comments.AddAsync(newComment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ ViewModels/Home/PostCommentVM.cs
@@ -0,0 +1,8 @@
+﻿namespace DMV_Connect.ViewModels.Home
+{
+    public class PostCommentVM
+    {
+        public int PostId { get; set; }
+        public string Content { get; set; }
+    }
+}
