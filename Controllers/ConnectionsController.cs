094416e1e0e2bfa674c74de4ab892593196a987c
dave-evans-dmv <dave.evans@dmv.virginia.gov>
2025-10-06 15:41:24 -0400

    Updated Connections Functionality & Tables

---
+++ Controllers/ConnectionsController.cs
@@ -0,0 +1,30 @@
+﻿using DMVConnect.Controllers.Base;
+using DMVConnect.Data.Interfaces;
+using DMVConnect.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMVConnect.Controllers
+{
+    public class ConnectionsController : BaseController
+    {
+        public readonly IConnectionsService _connectionsService;
+
+        public ConnectionsController(IConnectionsService connectionsService)
+        {
+            _connectionsService = connectionsService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NavigateToConnections()
+        {
+            var loggedInUserId = GetUserId();
+            if (loggedInUserId == null) return RedirectToLogin();
+
+            return RedirectToAction("Index", "Connections");
+        }
+    }
+}
---
+++ ViewComponents/SuggestedConnectionsViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using DMVConnect.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using DMVConnect.Controllers.Base;
+using DMVConnect.ViewModels.Connections;
+
+namespace DMVConnect.ViewComponents
+{
+    public class SuggestedConnectionsViewComponent : ViewComponent
+    {
+        private readonly IConnectionsService _connectionsService;
+
+        public SuggestedConnectionsViewComponent(IConnectionsService connectionsService)
+        {
+            _connectionsService = connectionsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var loggedInUserId = ((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(loggedInUserId))
+            {
+                return null;
+            }
+            var userId = int.Parse(loggedInUserId);
+            var suggestedConnections = await _connectionsService.GetSuggestedConnectionsAsync(userId);
+            var suggestedConnectionsVM = suggestedConnections.Select(n => new UserWithConnectionsCountVM()
+            {
+                UserId = n.User.Id,
+                FullName = n.User.FullName,
+                ProfilePictureUrl = n.User.ProfilePictureUrl,
+                ConnectionCount = n.ConnectionCount
+            }).ToList();
+            return View(suggestedConnectionsVM);
+        }
+    }
+}
